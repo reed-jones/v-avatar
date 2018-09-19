b2565a2dc1bce26d3e4c6377ff7679f4abc0a69c
Reed Jones <reedjones@reedjones.com>
2018-09-18 20:22:58 -0600

    initial commit

---
+++ README.md
@@ -0,0 +1,16 @@
+# v-avatar
+
+## Project setup
+```
+yarn install
+```
+
+### Compiles and hot-reloads for development
+```
+yarn run serve
+```
+
+### Compiles and minifies for production
+```
+yarn run build
+```
---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <v-avatar
+      mode="initials"
+      :size='128'
+      name="Reed Jones" />
+  </div>
+</template>
+
+<script>
+import VAvatar from './components/VAvatar'
+
+export default {
+  name: 'app',
+  components: {
+    VAvatar
+  }
+}
+</script>
+<style lang="stylus">
+#app
+  font-family 'Avenir', Helvetica, Arial, sans-serif
+  -webkit-font-smoothing antialiased
+  -moz-osx-font-smoothing grayscale
+  text-align center
+  color #2c3e50
+  margin-top 60px
+</style>
